ac8826e46a06ea768d6bd6aa8c2c63a65b7fa14c
FlorianKomnino <florian.komnino@outlook.it>
2022-12-23 16:56:44 +0100

    created appTitle.vue and added title to the page, with css styling

---
+++ florian-boolean-app/src/components/appTitle.vue
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<template>
+    <header>
+        <h1>
+            La mia prima app con Vite!
+        </h1>
+    </header>
+</template>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
